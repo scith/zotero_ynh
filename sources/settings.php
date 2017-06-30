29b545da67505983958b2cbd2ea436225f935b12
scith <contact@scith.ovh>
2017-06-30 18:58:07 +0200

    Fix

--- sources/settings.php
+++ sources/settings.php
@@ -19,7 +19,7 @@ $fetchlimit = 20;   // [default=20]; how many items to fetch with each API call 
 
 
 // I know, those are not API settings, but I need to know anyway...
-$data_dir  = 'zotero';     // [default="data/zotero"]; directory with sync data, either relative to the script's location or absolute path (on the same server though!). (NB: due to the way zotero sync works, this path ends most definetely in "/zotero)
+$data_dir  = 'data';     // [default="data/zotero"]; directory with sync data, either relative to the script's location or absolute path (on the same server though!). (NB: due to the way zotero sync works, this path ends most definetely in "/zotero)
 $cache_dir = 'cache';           // [default="cache"]; cache directory, relative or absolute. must be writable by the script. will attempt to create if not found
 $cache_age = 0;                 // [default=0]; in minutes. files will not be re-read from the sync storage to the file cache if they have already been grabbed within this time period. Also, cache folders that are older than this will be purged from the cache. set to 0 if you want to empty cache after each use (NB: if the attachment in question is a web snapshot and can be accessed by web the attachment will not be purged until the next use of the script)
 $cache_base_URL = "";           // [default=""]; if the cache directory is NOT a subdirectory of the directory which contains the script AND it (the cache) is web accessible, provide its URL here for use to display web snapshots. if the cache directory is not a subdirectory of the script's own directory and nothing it provided here, display of web snapshots will not work (all other attachment types are unaffected) 
--- sources/webdav_server.php
+++ sources/webdav_server.php
@@ -1,13 +1,13 @@
 <?php
 
-require '../settings.php';
+require 'settings.php';
 
-chdir ("../inc");
+chdir (dirname(__FILE__) . "/inc");
 
 require 'include.php';
 require_once "HTTP/WebDAV/Server/Filesystem.php";
 $server = new HTTP_WebDAV_Server_Filesystem();
 
-$server->ServeRequest( dirname( dirname(__FILE__) . '/' . $data_dir ) );
+$server->ServeRequest( dirname(__FILE__) . '/' . $data_dir );
 
 ?>
